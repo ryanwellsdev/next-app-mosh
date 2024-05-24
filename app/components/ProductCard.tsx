79b95fc39f8b4c5cc9577b5dae95048ce2d48ea4
ryanwellsdev <ryanwellsdev@gmail.com>
2024-05-24 16:17:35 -0600

    continuing

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import AddToCart from './AddToCart'
-import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
     return (
-        <div className={styles.card}>
+        <div >
             <AddToCart />
         </div>
     )
