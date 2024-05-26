751b471d1e0a98755c1cd635708737d85dcc6eb0
ryanwellsdev <ryanwellsdev@gmail.com>
2024-05-26 03:25:21 -0600

    styling

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -8,21 +8,58 @@ interface CloudinaryResult {
 
 const UploadPage = () => {
     const [publicId, setPublicId] = useState('')
+
     return (
         <>
-            {publicId &&
-                <CldImage src={publicId} width={270} height={180} alt='graphic' />}
-            <CldUploadWidget uploadPreset="c3ap1ykr"
-                onUpload={(result, widget) => {
-                    if (result.event !== 'success') return;
+            {publicId && (
+                <CldImage src={publicId} width={270} height={180} alt='graphic' />
+            )}
+
+            <CldUploadWidget
+                uploadPreset="c3ap1ykr"
+                options={{
+                    sources: ['local'],
+                    maxFiles: 5,
+                    styles: {
+                        palette: {
+                            window: "#000000",
+                            sourceBg: "#000000",
+                            windowBorder: "#8E9FBF",
+                            tabIcon: "#FFFFFF",
+                            inactiveTabIcon: "#8E9FBF",
+                            menuIcons: "#2AD9FF",
+                            link: "#08C0FF",
+                            action: "#336BFF",
+                            inProgress: "#00BFFF",
+                            complete: "#33ff00",
+                            error: "#EA2727",
+                            textDark: "#000000",
+                            textLight: "#FFFFFF"
+                        },
+                        fonts: {
+                            default: null,
+                            "'Space Mono', monospace": {
+                                url: "https://fonts.googleapis.com/css?family=Space+Mono",
+                                active: true
+                            }
+                        }
+                    }
+                }}
+                onUpload={(result) => {
+                    if (result.event !== 'success') return
                     const info = result.info as CloudinaryResult
                     setPublicId(info.public_id)
                     console.log(result)
-                }}>
-                {({ open }) =>
+                }}
+            >
+                {({ open }) => (
                     <button
                         className='btn btn-primary'
-                        onClick={() => open()}>Upload</button>}
+                        onClick={() => open()}
+                    >
+                        Upload
+                    </button>
+                )}
             </CldUploadWidget>
         </>
     )
